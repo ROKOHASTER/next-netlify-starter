703092beda243bf48546b495caa9709e4c091b50
ROKOHASTER <protocolodecuenta03@gmail.com>
2024-06-30 12:27:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,11 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
+    <style>
+     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+    </style>
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
@@ -12,9 +17,13 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
       </main>
 
       <Footer />
